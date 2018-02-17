a1280e21c7c77f8ab675cdf4ed80370ec8412ced
Alvin Joy Charles <alvinjoycharles@gmail.com>
2018-02-17 18:45:45 +1100

    api working with inputs (but retrieved json is not stored

--- weather.go
+++ weather.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	//"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strconv"
 )
@@ -41,7 +44,36 @@ func getInput() (string, string, int) {
 	return forecast_type, unit_type, days
 }
 
+func useWeatherApi(url string) []byte {
+	client := http.Client{}
+	request, _ := http.NewRequest(http.MethodGet, url, nil)
+	res, _ := client.Do(request)
+	body, _ := ioutil.ReadAll(res.Body)
+	return body
+	//_ = json.Unmarshal(body)
+}
+
+// returns the weather information as json based on the input
+func getWeatherInfo(forecast_type string, unit_type string, forecast_days int,
+	api_key string) string {
+
+	url := "https://extension.codecadets.com/api/current/?key=" + api_key
+	if forecast_type == "f" {
+		url = "https://extension.codecadets.com/api/forecast/?key=" + api_key
+		url += "&days=" + strconv.Itoa(forecast_days)
+	}
+
+	response := useWeatherApi(url)
+
+	fmt.Println(string(response))
+
+	return "foo"
+}
+
 func main() {
+	api_key := "fb171458a2e430573cd3edc90962b473"
+
 	fmt.Println("Welcome to ASCIIWeather!")
 	forecast_type, unit_type, forecast_days := getInput()
+	_ = getWeatherInfo(forecast_type, unit_type, forecast_days, api_key)
 }
